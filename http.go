ad4a6fbb0e565bf255c896be8b8870009215a91c
Jared Campbell <johndoe@example.com>
2023-06-14 21:24:57 -0500

    Add basic logging middleware

--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 
@@ -9,18 +10,38 @@ import (
 
 type server struct{}
 
+// NewHTTPServer creates an http server
 func NewHTTPServer(addr string) *http.Server {
 	s := &server{}
 	r := mux.NewRouter()
 
+	// setup handlers
 	r.HandleFunc("/", s.handleRoot).Methods("GET")
+
+	// register middleware
+	var handler http.Handler = r
+	handler = logRequestHandler(handler)
 
 	return &http.Server{
 		Addr:    addr,
-		Handler: r,
+		Handler: handler,
 	}
 }
 
+// logRequestHandler is a middleware that writes an http log for each request
+func logRequestHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+
+		url := r.URL.String()
+		method := r.Method
+
+		log.Printf("%s :: %s", url, method)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// handleRoot handles the root path "/"
 func (s *server) handleRoot(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("tmpl/home.html")
 	t.Execute(w, nil)
